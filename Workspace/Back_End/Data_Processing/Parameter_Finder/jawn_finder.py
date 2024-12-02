949bf241b64dd18d751afbdba2eb52af655b866b
Pankelele <115100407+Pankelele@users.noreply.github.com>
2024-12-02 23:45:16 +0100

    Reformated jawn_finder.py to PEP8 convention and updated its documentation.

--- Workspace/Back_End/Data_Processing/Parameter_Finder/jawn_finder.py
+++ Workspace/Back_End/Data_Processing/Parameter_Finder/jawn_finder.py
@@ -4,27 +4,36 @@ from .parameter_finder import ParameterFinder
 
 
 class JawnFinder(ParameterFinder):
+    """Klasa odpowiedzialna za wykrycie ziewania."""
+
     def __init__(self, yawn_threshold):
-        self.mouth_indices = np.array([[37, 84], [0, 17], [267, 314], [62,29]])
+        """Konstruktor klasy JawnFinder."""
+        self.mouth_indices = np.array([[37, 84], [0, 17], [267, 314], [62, 29]])
         self.yawn_counter = 0
-        self.yawn_memory = np.array(np.zeros(10),dtype='bool')
+        self.yawn_memory = np.array(np.zeros(10), dtype='bool')
         self.yawn_threshold = yawn_threshold
 
+    def find_parameter(self, face_coords) -> tuple:
+        """
+        Metoda do zwracania, czy osoba ziewa oraz liczbę wykrytych ziewnięć.
 
-    def find_parameter(self, face_coords):
+        :param face_coords: Wynik działania funkcji process od mediapipe
+        :type face_coords: Union
+        :return: Flaga, czy osoba ziewa oraz liczbę wykrytych ziewnięć
+        :rtype: tuple
+        """
         jawn_ratios = self._find_jawn_ratio(face_coords)
         is_jawning = self._detect_yawn(jawn_ratios)
         return is_jawning, self.yawn_counter
 
-
-    def _find_jawn_ratio(self,face_coords):
+    def _find_jawn_ratio(self, face_coords) -> list:
         """
-        Metoda do obliczania szerokości otwarcia ust
-        #TODO Dokumentacja
-        :param face_coords: Wynik działania modelu mediapipe
-        :type face_coords:
-        :return:
-        :rtype:
+        Metoda do obliczania stosunku wysokości otwarcia ust do jej szerokości.
+
+        :param face_coords: Wynik działania funkcji process od mediapipe
+        :type face_coords: Union
+        :return: Stosunek wysokości otwarcia ust do jej szerokości dla wszystkich wykrytych twarzy
+        :rtype: list
         """
         all_delta_ver_dist = np.array([])
         all_faces_jawn_ratio = list()
@@ -48,16 +57,16 @@ class JawnFinder(ParameterFinder):
             return [0]
 
     @staticmethod
-    def _calculate_distance(face_mesh, pair):
+    def _calculate_distance(face_mesh, pair: list) -> float:
         """
+        Metoda do obliczania odległości między dwoma punktami.
 
-
-        :param face_mesh:
-        :type face_mesh:
-        :param pair:
-        :type pair:
-        :return:
-        :rtype:
+        :param face_mesh: Pojedyńcza twarz znaleziona przez mediapipe
+        :type face_mesh: Union
+        :param pair: Para wskaźników na twarzy, względem której zostanie wyliczona odległość.
+        :type pair: np.array
+        :return: Odległość między dwoma punktami
+        :rtype: float
         """
         y2 = face_mesh.landmark[pair[0]].y
         x2 = face_mesh.landmark[pair[0]].x
@@ -66,11 +75,11 @@ class JawnFinder(ParameterFinder):
         delta_ver_dist = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
         return delta_ver_dist
 
-    def _detect_yawn(self, yawn_ratio):
+    def _detect_yawn(self, yawn_ratio: float) -> bool:
         """
-
-        :return:
-        :rtype:
+        Metoda do wykrywania, czy użytkownik obecnie ziewa oraz zliczania liczby ziewnięć.
+        :return: Wartość bool, czy użytkownik aktualnie ziewa
+        :rtype: bool
         """
         self.yawn_memory = np.roll(self.yawn_memory, -1)
         self.yawn_memory[-1] = False
