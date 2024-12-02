e8504d6b5d0812fff8eefd7ef2bc6bf85a7b53a6
Pankelele <115100407+Pankelele@users.noreply.github.com>
2024-12-02 23:35:51 +0100

    Reformated face_angle_finder.py to PEP8 convention and updated its documentation.

--- Workspace/Back_End/Data_Processing/Parameter_Finder/face_angle_finder.py
+++ Workspace/Back_End/Data_Processing/Parameter_Finder/face_angle_finder.py
@@ -1,27 +1,40 @@
-import numpy as np
 import math
+
+import numpy as np
 
 from .parameter_finder import ParameterFinder
 
-class FaceAngleFinder(ParameterFinder):
-    def __init__(self):
-        self.face_oval_indices = np.array([[109,148],[10,152],[338,377]])
 
-    def find_parameter(self, face_coords):
+class FaceAngleFinder(ParameterFinder):
+    """Klasa odpowiedzialna za znalezienie kąta pochylenia twarzy."""
+
+    def __init__(self):
+        """Konstruktor klasy FaceAngleFinder."""
+        self.face_oval_indices = np.array([[109, 148], [10, 152], [338, 377]])
+
+    def find_parameter(self, face_coords) -> float:
+        """
+        Metoda do zwrócenia kątu pochylenia twarzy.
+
+        :param face_coords: Wynik działania funkcji process od mediapipe
+        :type face_coords: Union
+        :return: Kąt pochylenia twarzy
+        :rtype: float
+        """
         face_angle = self._find_face_angle(face_coords)
         return face_angle
 
-    def _find_face_angle(self, face_coords):
+    def _find_face_angle(self, face_coords) -> float:
         """
-
-        #TODO Dokumentacja
-        :param face_coords: Wynik działania modelu mediapipe
-        :type face_coords:
-        :return:
-        :rtype:
+        Metoda do wyliczenia kątu pochylenia twarzy.
+        :param face_coords: Wynik działania funkcji process od mediapipe
+        :type face_coords: Union
+        :return: Kąt pochylenia twarzy
+        :rtype: float
         """
         all_faces_face_angle = np.array([])
 
+        # Dla każdej pary wskaźników na twarzy obliczenie ich pochylenia względem pionu i obliczenie średniej z nich
         if face_coords.multi_face_landmarks:
             for face_mesh in face_coords.multi_face_landmarks:
                 for pair in self.face_oval_indices[0:-1]:
@@ -29,22 +42,22 @@ class FaceAngleFinder(ParameterFinder):
                     all_faces_face_angle = np.append(all_faces_face_angle, face_angle)
 
             face_angle = np.mean(all_faces_face_angle)
-
             return face_angle
+
         else:
             return 0
 
     @staticmethod
-    def _calculate_angle(face_mesh, pair):
+    def _calculate_angle(face_mesh, pair: np.array) -> float:
         """
+        Metoda do wyznaczenia pochylenia dwóch wskaźników twarzy.
 
-
-        :param face_mesh:
-        :type face_mesh:
-        :param pair:
-        :type pair:
-        :return:
-        :rtype:
+        :param face_mesh: Pojedyńcza twarz znaleziona przez mediapipe
+        :type face_mesh: Union
+        :param pair: Para wskaźników na twarzy, względem której zostanie wyliczone ich pochylenie względem pionu
+        :type pair: np.array
+        :return: Kąt pochylenia pary punktów względem pionu
+        :rtype: float
         """
         x2 = face_mesh.landmark[pair[0]].x
         y2 = face_mesh.landmark[pair[0]].y
@@ -58,7 +71,7 @@ class FaceAngleFinder(ParameterFinder):
         delta_y = y2 - y1
         delta_z = abs(z2 - z1)
 
-        denominator = (delta_x * delta_x + delta_y * delta_y)**(1/2)
-        face_angle = math.atan2(delta_z,denominator)*180/math.pi * delta_y/abs(delta_y)
+        denominator = (delta_x * delta_x + delta_y * delta_y) ** (1 / 2)
+        face_angle = math.atan2(delta_z, denominator) * 180 / math.pi * delta_y / abs(delta_y)
 
         return face_angle
--- Workspace/Back_End/Data_Processing/Parameter_Finder/parameter_finder.py
+++ Workspace/Back_End/Data_Processing/Parameter_Finder/parameter_finder.py
@@ -1,10 +1,13 @@
 import abc
 
 class ParameterFinder(metaclass=abc.ABCMeta):
+    """Klasa odpowiedzialna za znalezienie konkretnego parametru"""
     @abc.abstractmethod
     def __init__(self):
+        """Konstruktor klasy"""
         pass
 
     @abc.abstractmethod
     def find_parameter(self, face_coords):
+        """Metoda do zwrócenia szukanego parametru na podstawie współrzędnych wskaźników na twarzy."""
         pass
