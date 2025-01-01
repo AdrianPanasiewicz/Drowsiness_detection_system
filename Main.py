Rename and adjust "jawn" to "yawn," fix PERCLOS calculation.
Renamed instances of "jawn" to "yawn" for consistency and correctness, including classes, methods, and variables. Adjusted PERCLOS calculation by computing the mean of paired values. Updated GUI and Main.py to reflect the changes while improving parameter tracking and display.

Main.py
@@ -10,19 +10,12 @@ if __name__ == "__main__":
             processed_frame, face_mesh_coords = image_processor.process_face_image(frame)
 
             perclos, ear = find_perclos.find_parameter(face_mesh_coords)
-            is_jawning, jawn_counter, mar = find_jawn.find_parameter(face_mesh_coords)
+            is_jawning, yawn_counter, mar = find_yawn.find_parameter(face_mesh_coords)
             roll, pitch = find_face_tilt.find_parameter(face_mesh_coords)
             saccade_velocity = find_saccade_velocity.find_parameter(face_mesh_coords)
 
             # Utils.render_face_coordinates(coordinates_parser, face_plotter, face_mesh_coords)
 
-            cv2.putText(processed_frame, f"Saccade speed: {round(saccade_velocity, 3)}", (15, 60), *text_parameters)
-            cv2.putText(processed_frame, f"PERCLOS: {int(perclos*100)}%", (15, 90), *text_parameters)
-            cv2.putText(processed_frame, f"Jawn: {is_jawning}", (15, 120), *text_parameters)
-            cv2.putText(processed_frame, f"Jawn counter: {jawn_counter}", (15, 150), *text_parameters)
-            cv2.putText(processed_frame, f"Roll: {round(roll, 2)}", (15, 180), *text_parameters)
-            cv2.putText(processed_frame, f"Pitch: {round(pitch, 2)}", (15, 210), *text_parameters)
-            cv2.putText(processed_frame, f"FPS: {int(fps)}", (15, 240), *text_parameters)
             cv2.imshow('Drowsiness detection', processed_frame)
 
             packet = {
@@ -34,7 +27,7 @@ if __name__ == "__main__":
                 "PERCLOS": perclos
             }
 
-            gui_display.update_parameters(mar, is_jawning, roll, pitch, ear, perclos)
+            gui_display.update_parameters(mar, is_jawning, roll, pitch, ear, perclos, yawn_counter, fps)
 
             sql_saver.save_to_csv(packet)
 
@@ -68,11 +61,11 @@ if __name__ == "__main__":
     except TypeError as e:
         raise TypeError('Nie udało się uzyskać dostępu do kamery lub kamera nie istnieje') from e
 
-    perclos_threshold = 0.4
+    perclos_threshold = 0.2
     yawn_threshold = 0.55
 
     find_perclos = perclos_finder.PerclosFinder(perclos_threshold)
-    find_jawn = jawn_finder.JawnFinder(yawn_threshold)
+    find_yawn = yawn_finder.YawnFinder(yawn_threshold)
     find_face_tilt = face_angle_finder.FaceAngleFinder()
     find_saccade_velocity = saccade_speed_velocity.SaccadeVelocityFinder()
     # saccs = np.zeros(1)

Workspace/Back_End/Data_Processing/Parameter_Finder/yawn_finder.py
@@ -3,11 +3,11 @@ import numpy as np
 from .parameter_finder import ParameterFinder
 
 
-class JawnFinder(ParameterFinder):
+class YawnFinder(ParameterFinder):
     """Klasa odpowiedzialna za wykrycie ziewania."""
 
     def __init__(self, yawn_threshold: float):
-        """Konstruktor klasy JawnFinder."""
+        """Konstruktor klasy YawnFinder."""
         self.mouth_indices = np.array([[37, 84], [0, 17], [267, 314], [62, 29]])
         self.yawn_counter = 0
         self.yawn_memory = np.array(np.zeros(10), dtype='bool')
@@ -22,11 +22,11 @@ class JawnFinder(ParameterFinder):
         :return: Flaga, czy osoba ziewa oraz liczbę wykrytych ziewnięć
         :rtype: tuple
         """
-        jawn_ratios = self._find_jawn_ratio(face_coords)
-        is_jawning = self._check_for_yawn(jawn_ratios)
-        return is_jawning, self.yawn_counter, jawn_ratios[0] if face_coords.multi_face_landmarks else 0.0
+        yawn_ratios = self._find_yawn_ratio(face_coords)
+        is_jawning = self._check_for_yawn(yawn_ratios)
+        return is_jawning, self.yawn_counter, yawn_ratios[0] if face_coords.multi_face_landmarks else 0.0
 
-    def _find_jawn_ratio(self, face_coords) -> list:
+    def _find_yawn_ratio(self, face_coords) -> list:
         """
         Metoda do obliczania stosunku wysokości otwarcia ust do jej szerokości.
 
@@ -36,12 +36,12 @@ class JawnFinder(ParameterFinder):
         :rtype: list
         """
         all_delta_ver_dist = np.array([])
-        all_faces_jawn_ratio = list()
+        all_faces_yawn_ratio = list()
 
         if face_coords.multi_face_landmarks:
             for face_mesh in face_coords.multi_face_landmarks:
                 for pair in self.mouth_indices[0:-1]:
-                    delta_ver_dist = JawnFinder._calculate_distance(face_mesh, pair)
+                    delta_ver_dist = YawnFinder._calculate_distance(face_mesh, pair)
                     all_delta_ver_dist = np.append(all_delta_ver_dist, delta_ver_dist)
 
                 hor_distance = self._calculate_distance(face_mesh, self.mouth_indices[-1])
@@ -50,9 +50,9 @@ class JawnFinder(ParameterFinder):
                 mean_ver_distance = np.mean(all_delta_ver_dist)
 
                 jawn_ratio = mean_ver_distance / hor_distance
-                all_faces_jawn_ratio.append(jawn_ratio)
+                all_faces_yawn_ratio.append(jawn_ratio)
 
-            return all_faces_jawn_ratio
+            return all_faces_yawn_ratio
         else:
             return [0]
 