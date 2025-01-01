82a5d631c81ad944f823488e2d88f6a648807526
Pankelele <115100407+Pankelele@users.noreply.github.com>
2025-01-01 23:44:52 +0100

    Added docstrings to GUI and 3D face visualization.

--- Workspace/Front_End/face_plotter.py
+++ Workspace/Front_End/face_plotter.py
@@ -1,52 +1,83 @@
 class FacePlotter:
     def __init__(self, figure, axes3d, canvas, root):
         """
-        Inicjalizacja klasy FacePlotter.
+        Inicjalizacja obiektu FacePlotter, odpowiedzialnego za wyświetlanie i
+        aktualizację punktów na twarzy w przestrzeni 3D w interfejsie Tkinter.
+
+        :param figure: Obiekt klasy Figure (Matplotlib), w którym rysowany jest wykres.
+        :type figure: matplotlib.figure.Figure
+        :param axes3d: Oś 3D (Axes3D) z biblioteki Matplotlib, na której będą rysowane punkty.
+        :type axes3d: mpl_toolkits.mplot3d.axes3d.Axes3D
+        :param canvas: Obiekt FigureCanvasTkAgg, umożliwiający osadzenie wykresu w Tkinter.
+        :type canvas: matplotlib.backends.backend_tkagg.FigureCanvasTkAgg
+        :param root: Główne okno (widget) Tkinter, wykorzystywane do wywoływania metody after().
+        :type root: tkinter.Tk lub tkinter.Toplevel
         """
 
+        # Słowniki przechowujące współrzędne X, Y oraz Z dla wykrywanych elementów twarzy
         self.x_dict_all = dict()
         self.y_dict_all = dict()
         self.z_dict_all = dict()
 
+        # Referencje do obiektów Matplotlib i Tkinter
         self.fig = figure
         self.ax = axes3d
         self.canvas = canvas
         self.root = root
 
+        # Flaga określająca, czy animacja (aktualizacja wykresu) jest uruchomiona
         self._animation_running = False
 
     def start_animation(self, interval=100):
+        """
+        Rozpoczyna animację wykresu w ustalonym odstępie czasowym (co 'interval' milisekund).
+        Wywołuje wewnętrzną metodę _update_plot() przy pomocy metody Tkinter after().
+
+        :param interval: Częstotliwość odświeżania w milisekundach (np. 100 = 10 FPS).
+        :type interval: int
+        """
         self._animation_running = True
         self._update_plot(interval)
 
     def stop_animation(self):
+        """
+        Zatrzymuje animację, uniemożliwiając dalsze aktualizacje wykresu.
+        """
         self._animation_running = False
 
     def update_xyz_coords(self, x_list: list, y_list: list, z_list: list, name: str):
         """
-        Metoda do zaktualizowania wartości współrzędnych danego fragmentu na twarzy w tej klasie, a jeśli nie istnieje
-        to stworzenie nowego rekordu do ich zapisu.
-        :param x_list: Lista współrzędnych X dla wskaźników na twarzy
+        Aktualizuje lub dodaje nowe współrzędne (X, Y, Z) dla wybranego fragmentu twarzy.
+        Jeżeli wpis pod kluczem 'name' nie istnieje, tworzy nowy; w przeciwnym razie aktualizuje istniejący.
+
+        :param x_list: Lista współrzędnych X danego fragmentu twarzy.
         :type x_list: list
-        :param y_list:Lista współrzędnych Y dla wskaźników na twarzy
+        :param y_list: Lista współrzędnych Y danego fragmentu twarzy.
         :type y_list: list
-        :param z_list: Lista współrzędnych Z dla wskaźników na twarzy
+        :param z_list: Lista współrzędnych Z danego fragmentu twarzy.
         :type z_list: list
-        :param name: Nazwa aktualizowanego fragmentu twarzy
+        :param name: Nazwa fragmentu twarzy (np. "LEFT_EYE", "MOUTH").
         :type name: str
         """
-
         self.x_dict_all.update({name: x_list})
         self.y_dict_all.update({name: y_list})
         self.z_dict_all.update({name: z_list})
 
     def _update_plot(self, interval=33):
         """
+        Metoda wewnętrzna odpowiedzialna za cykliczną aktualizację wykresu. Czyści istniejący
+        rysunek, ustawia parametry osi, a następnie rysuje nowe punkty na bazie danych ze
+        słowników współrzędnych. Po wykonaniu rysowania, planuje kolejne wywołanie samej siebie
+        po określonym czasie 'interval', o ile animacja nie została zatrzymana.
 
+        :param interval: Częstotliwość aktualizacji w milisekundach.
+        :type interval: int
         """
+        # Sprawdzenie, czy animacja nie została zatrzymana
         if not self._animation_running:
             return
 
+        # Czyszczenie obszaru rysowania i nadanie limitów osi
         self.ax.cla()
         self.ax.set(
             xlim=(0, 1),
@@ -57,8 +88,11 @@ class FacePlotter:
             zlabel='Height'
         )
 
+        # Rysowanie wszystkich punktów zarejestrowanych w słownikach
         for key in self.x_dict_all:
+            # Dla każdej osoby w danym fragmencie twarzy
             for person_index in range(len(self.x_dict_all[key])):
+                # Dla każdej 'linii' (łańcucha punktów) we współrzędnych
                 for line_index in range(len(self.x_dict_all[key][person_index])):
                     line_color = self._select_color(key, person_index)
                     x_vals = self.x_dict_all[key][person_index][line_index]
@@ -66,65 +100,85 @@ class FacePlotter:
                     z_vals = self.z_dict_all[key][person_index][line_index]
                     self.ax.plot(x_vals, z_vals, y_vals, color=line_color)
 
+        # Odświeżenie widoku w interfejsie
         self.canvas.draw()
+
+        # Zaplanowanie kolejnej aktualizacji po 'interval' milisekundach
         self.root.after(interval, self._update_plot, interval)
 
     @staticmethod
     def _select_color(key: str, person_index: int) -> str:
         """
-        Metoda odpowiedzialna za wybór koloru, w którym dana część twarzy jest rysowana.
-        :param key: Nazwa fragmentu twarzy
+        Wybiera kolor (w formacie heksadecymalnym) na podstawie nazwy fragmentu twarzy
+        (parametr 'key') oraz indeksu osoby, jeśli w kadrze może być wiele twarzy.
+
+        :param key: Nazwa fragmentu twarzy (np. "LEFT_EYE", "MOUTH").
         :type key: str
-        :param person_index: Indeks osoby, jeśli jest więcej niż jedna osoba obecna na kamerze.
+        :param person_index: Indeks osoby, jeśli wykryto wiele osób (domyślnie 0 przy jednej twarzy).
         :type person_index: int
-        :return: String z odpowiednim kolorem dla fragmentu twarzy
+        :return: Kolor w formacie szesnastkowym np. "#ff0000".
         :rtype: str
         """
+        # Przykładowe mapowanie kolorów w zależności od fragmentu twarzy i indeksu osoby
         if key.upper() == "LEFT_EYE":
-            line_color = FacePlotter._format_rgb_string(15 + 50 * person_index, 15 + 50 * person_index,
-                                                        160 - 50 * person_index)
+            line_color = FacePlotter._format_rgb_string(
+                15 + 50 * person_index,
+                15 + 50 * person_index,
+                160 - 50 * person_index
+            )
         elif key.upper() == "RIGHT_EYE":
-            line_color = FacePlotter._format_rgb_string(15 + 50 * person_index, 15 + 50 * person_index,
-                                                        160 - 50 * person_index)
+            line_color = FacePlotter._format_rgb_string(
+                15 + 50 * person_index,
+                15 + 50 * person_index,
+                160 - 50 * person_index
+            )
         elif key.upper() == "MOUTH":
-            line_color = FacePlotter._format_rgb_string(160 - 50 * person_index, 15 + 50 * person_index,
-                                                        15 + 50 * person_index)
+            line_color = FacePlotter._format_rgb_string(
+                160 - 50 * person_index,
+                15 + 50 * person_index,
+                15 + 50 * person_index
+            )
         elif key.upper() == "LEFT_IRIS":
-            line_color = FacePlotter._format_rgb_string(15, 15 + 50 * person_index, 15 + 50 * person_index)
+            line_color = FacePlotter._format_rgb_string(
+                15,
+                15 + 50 * person_index,
+                15 + 50 * person_index
+            )
         elif key.upper() == "RIGHT_IRIS":
-            line_color = FacePlotter._format_rgb_string(15, 15 + 50 * person_index, 15 + 50 * person_index)
+            line_color = FacePlotter._format_rgb_string(
+                15,
+                15 + 50 * person_index,
+                15 + 50 * person_index
+            )
         else:
-            line_color = FacePlotter._format_rgb_string(150 - 50 * person_index, 15, 150 - 50 * person_index)
+            line_color = FacePlotter._format_rgb_string(
+                150 - 50 * person_index,
+                15,
+                150 - 50 * person_index
+            )
 
         return line_color
 
     @staticmethod
     def _format_rgb_string(rval: int, gval: int, bval: int) -> str:
         """
-        Metoda do utworzenia string rgb na podstawie żądanych wartości koloru czerwonego, zielonego i niebieskiego.
-        :param rval: Wartość koloru czerwonego
+        Konwertuje wartości składowych RGB (czerwonej, zielonej i niebieskiej) na format
+        szesnastkowy (#RRGGBB), gdzie każda składowa mieści się w zakresie [0, 255].
+
+        :param rval: Wartość składowej czerwonej.
         :type rval: int
-        :param gval: Wartość koloru zielonego
+        :param gval: Wartość składowej zielonej.
         :type gval: int
-        :param bval: Wartość koloru niebieskiego
+        :param bval: Wartość składowej niebieskiej.
         :type bval: int
-        :return: String w formacie rgb
+        :return: Kolor w formacie szesnastkowym (np. "#0f00a3").
         :rtype: str
         """
-        # Przekształcenie wartości całkowitych kolorów na odpowiadające im wartości hex, zapisane jako string
-        red = str(hex(rval % 256))[2:]
-        green = str(hex(gval % 256))[2:]
-        blue = str(hex(bval % 256))[2:]
+        # Normalizacja wartości składowych (mod 256) i zamiana na zapis szesnastkowy bez prefiksu "0x"
+        red = f"{rval % 256:02x}"
+        green = f"{gval % 256:02x}"
+        blue = f"{bval % 256:02x}"
 
-        # W przypadku, gdy wartość koloru można zapisać jako liczba jednocyfrowa w systemie szesnastkowym, to dopełnić
-        # dodatkowym zerem
-        if len(red) == 1:
-            red = '0' + red
-        if len(green) == 1:
-            green = '0' + green
-        if len(blue) == 1:
-            blue = '0' + blue
-
-        # Stworzenie string rgb
+        # Złożenie całości w formacie #RRGGBB
         line_color = f"#{red}{green}{blue}"
         return line_color
--- Workspace/Front_End/gui.py
+++ Workspace/Front_End/gui.py
@@ -7,23 +7,37 @@ import os
 import cv2
 from Workspace.Front_End.face_plotter import FacePlotter
 
+
 class GUI:
+    """
+    Klasa GUI zarządza głównym oknem aplikacji, umożliwia wyświetlanie
+    podglądu z kamery, wizualizację 3D twarzy oraz prezentację obliczonych
+    parametrów (m.in. MAR, EAR, PERCLOS). Zapewnia także funkcjonalność
+    zmiany stylu interfejsu i wyświetlanie informacji o programie.
+    """
+
     def __init__(self):
-        # Flaga określająca, czy główna pętla programu powinna się uruchamiać.
+        """
+        Inicjalizuje główne okno aplikacji i wszystkie jego komponenty,
+        w tym ramki (frames), etykiety (labels) i płótna (canvas).
+        Ustawia także kolejki (Queue) do obsługi asynchronicznego
+        wyświetlania obrazu z kamery oraz zaktualizowanych parametrów.
+        """
+        # Flaga kontrolująca działanie głównej pętli programu.
         self.running = True
 
-        # Kolejki, w których będą przechowywane dane parametry i klatki obrazu do wyświetlenia w GUI.
+        # Kolejki do przechowywania danych o parametrach i obrazu z kamery.
         self.data_queue = queue.Queue()
         self.image_queue = queue.Queue()
 
-        # Przechowywana aktualna klatka z kamery (w formacie zgodnym z tkinter).
+        # Bieżący obraz z kamery, przetworzony dla tkinter.
         self.current_image = None
 
         # Ustawienie rozmiarów głównego okna.
         self.width = 1450
         self.height = 650
 
-        # Konfiguracja wyglądu (tryb ciemny/jasny/systemowy) za pomocą biblioteki customtkinter.
+        # Konfiguracja wyglądu interfejsu.
         customtkinter.set_appearance_mode("dark")
         customtkinter.set_default_color_theme("blue")
 
@@ -33,10 +47,10 @@ class GUI:
         self.window.title("System wykrywania senności")
         self.window.resizable(False, False)
 
-        # Ścieżka do bieżącego pliku (wykorzystywana np. do ładowania obrazów tła).
+        # Ścieżka bieżącego pliku – przydatna np. do ładowania zasobów (obrazów tła itp.).
         self.current_path = os.path.dirname(os.path.realpath(__file__))
 
-        # Ładowanie obrazu tła dla głównego okna i umieszczenie go jako tło za pomocą `place`.
+        # Ładowanie i ustawianie obrazu tła w głównym oknie (użycie .place).
         self.bg_image = customtkinter.CTkImage(
             Image.open(self.current_path + "/gray_bg_img.jpeg"),
             size=(self.width, self.height)
@@ -44,11 +58,10 @@ class GUI:
         self.bg_image_label = customtkinter.CTkLabel(self.window, text="", image=self.bg_image)
         self.bg_image_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        # Dolny panel (frame) z menu wyboru trybu wyglądu i przyciskiem informacji o programie.
+        # Dolny panel z listą wyboru trybu (ciemny/jasny/systemowy) i przyciskiem informacyjnym.
         self.bottom_frame = customtkinter.CTkFrame(self.window)
         self.bottom_frame.pack(side="bottom", fill="x")
 
-        # Menu wyboru trybu wyglądu (Ciemny/Jasny/Systemowy).
         self.appearance_mode_menu = customtkinter.CTkOptionMenu(
             self.bottom_frame,
             values=["Ciemny", "Jasny", "Systemowy"],
@@ -56,7 +69,6 @@ class GUI:
         )
         self.appearance_mode_menu.pack(side="left", padx=20, pady=10)
 
-        # Przycisk wyświetlający okno „O programie”.
         self.info_button = customtkinter.CTkButton(
             self.bottom_frame,
             text="O programie",
@@ -64,40 +76,44 @@ class GUI:
         )
         self.info_button.pack(side="right", padx=20, pady=10)
 
-        # Sekcja po lewej stronie okna – ramka (frame) wyświetlająca obraz z kamery.
+        # Ramka (frame) z lewej, przeznaczona na wyświetlanie obrazu z kamery.
         self.webcam_frame = customtkinter.CTkFrame(self.window)
         self.webcam_frame.pack(pady=20, padx=20, fill="both", expand=True, side="left")
 
-        # Etykieta – nagłówek informujący, że poniżej widać „Kamera”.
+        # Etykieta tytułowa w ramce kamery.
         self.webcam_label = customtkinter.CTkLabel(
-            self.webcam_frame, text="Kamera", font=("JetBrains Mono", 40, 'bold')
+            self.webcam_frame,
+            text="Kamera",
+            font=("JetBrains Mono", 40, 'bold')
         )
         self.webcam_label.pack(pady=20, padx=20)
 
-        # Obszar (Canvas), w którym będzie wyświetlany obraz z kamery.
+        # Obszar (Canvas) do wyświetlania bieżącego obrazu z kamery.
         self.webcam_canvas = customtkinter.CTkCanvas(self.webcam_frame, width=640, height=480)
         self.webcam_canvas.pack(pady=25, padx=5, anchor="center")
 
-        # Ramka (frame) po prawej stronie, w której umieszczamy wizualizację 3D.
+        # Ramka (frame) po prawej stronie do osadzenia wizualizacji 3D.
         self.plot_frame = customtkinter.CTkFrame(self.window)
         self.plot_frame.pack(pady=20, padx=20, fill="both", expand=True, side="right")
 
-        # Etykieta – nagłówek „Wizualizacja 3D twarzy”.
+        # Etykieta tytułowa dla wizualizacji 3D.
         self.plot_label = customtkinter.CTkLabel(
-            self.plot_frame, text="Wizualizacja 3D twarzy", font=("JetBrains Mono", 40, 'bold')
+            self.plot_frame,
+            text="Wizualizacja 3D twarzy",
+            font=("JetBrains Mono", 40, 'bold')
         )
         self.plot_label.pack(pady=20, padx=20)
 
-        # Tworzenie obiektu Figure z matplotlib i dodawanie do niej osi 3D.
+        # Inicjalizacja obiektu Figure (matplotlib) i dodanie osi 3D.
         self.fig = plt.Figure(figsize=(5, 4), dpi=100)
         self.ax = self.fig.add_subplot(111, projection='3d')
 
-        # Tworzenie widgetu FigureCanvasTkAgg – osadzenie matplotlib wewnątrz customtkinter.
+        # Osadzenie wykresu Matplotlib w interfejsie TKinter przez FigureCanvasTkAgg.
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
 
-        # Inicjalizacja klasy FacePlotter (odpowiedzialnej za rysowanie twarzy 3D) i uruchomienie animacji.
+        # Utworzenie instancji FacePlotter do wyświetlania twarzy 3D i uruchomienie animacji.
         self.face_plotter_inst = FacePlotter(
             figure=self.fig,
             axes3d=self.ax,
@@ -106,17 +122,19 @@ class GUI:
         )
         self.face_plotter_inst.start_animation(interval=33)
 
-        # Ramka (frame) do wyświetlania parametrów (po prawej, obok wizualizacji).
+        # Ramka (frame) do wyświetlania parametrów obliczanych na podstawie obrazu twarzy.
         self.params_frame = customtkinter.CTkFrame(self.window)
         self.params_frame.pack(pady=20, padx=20, fill="both", expand=True, side="right")
 
-        # Etykieta – nagłówek „Parametry”.
+        # Etykieta tytułowa w ramce parametrów.
         self.params_title = customtkinter.CTkLabel(
-            self.params_frame, text="Parametry:", font=("JetBrains Mono", 40, 'bold')
+            self.params_frame,
+            text="Parametry:",
+            font=("JetBrains Mono", 40, 'bold')
         )
         self.params_title.pack(pady=20, padx=20)
 
-        # Tekst wyjściowy – inicjalnie przykładowe wartości 0 lub „Brak”.
+        # Tekst wyświetlający przykładowe wartości na starcie.
         params_text = (
             f"MAR:\t\t 0\n"
             f"Ziewanie:\t\t Brak\n"
@@ -129,7 +147,7 @@ class GUI:
             f"FPS:\t\t 0\n"
         )
 
-        # Etykieta wyświetlająca wartości parametrów; używamy monospaced font (JetBrains Mono) i lewego justowania.
+        # Etykieta prezentująca parametry, używająca monospaced fontu i lewego justowania.
         self.params_label = customtkinter.CTkLabel(
             self.params_frame,
             text=params_text,
@@ -139,37 +157,41 @@ class GUI:
         )
         self.params_label.pack(pady=20, padx=20)
 
-        # Uruchomienie metod, które cyklicznie aktualizują obraz z kamery i parametry w GUI.
+        # Wywołanie metod cyklicznych, aktualizujących dane w GUI.
         self.update_webcam()
         self.update_labels()
 
-
     def show_info_window(self):
+        """
+        Wyświetla okno informacyjne „O programie”, zawierające dane na temat autora,
+        celu aplikacji i sposobu jej obsługi.
+        """
         info_window = customtkinter.CTkToplevel(self.window)
         info_window.title("O programie")
         info_window.geometry("500x380")
 
+        # Ustawienie parametru topmost, aby okno z informacjami było na wierzchu.
         info_window.attributes("-topmost", 1)
 
         info_test = """        Autor: Adrian Paweł Panasiewicz  
         Tytuł pracy dyplomowej: Projekt wstępny systemu bezpieczeństwa do wykrywania 
                                                 senności u pilotów bezzałogowych 
                                                 statków powietrznych  
-        
+
         Cel aplikacji:
         Aplikacja została zaprojektowana w celu monitorowania stanu senności 
         operatorów dronów w czasie rzeczywistym. Analizuje obraz z kamery 
         i wyświetla parametry takie jak PERCLOS, ziewanie, czy pochylenie 
         głowy. W przypadku wykrycia krytycznych wartości system generuje 
         alerty dźwiękowe i wizualne.
-        
+
         Jak obsługiwać aplikację:
         1. Uruchom plik wykonywalny aplikacji na systemie Windows 10/11.
         2. Podłącz kamerę zgodną z minimalnymi wymaganiami (720p, 30 FPS).
         3. Ustaw kamerę tak, aby rejestrowała twarz operatora w dobrych 
            warunkach oświetleniowych.
         4. Obserwuj dane wyjściowe na interfejsie graficznym aplikacji (GUI).
-        
+
         Uwaga:
         - Aplikacja obsługuje tylko jedną twarz w kadrze.
         - Stabilne oświetlenie i minimalne ruchy kamery poprawiają dokładność analizy.
@@ -185,18 +207,67 @@ class GUI:
         info_label.pack(pady=20, padx=20)
 
     def queue_image(self, image):
+        """
+        Dodaje nowy obraz (np. z kamery) do kolejki image_queue, skąd
+        będzie pobierany i wyświetlany w metodzie update_webcam().
+
+        :param image: Obraz w formacie kompatybilnym z OpenCV (np. BGR).
+        :type image: numpy.ndarray
+        """
         self.image_queue.put(image)
 
     def get_face_plotter(self):
+        """
+        Zwraca instancję klasy FacePlotter wykorzystywaną do wizualizacji 3D twarzy.
+
+        :return: Instancja FacePlotter.
+        :rtype: FacePlotter
+        """
         return self.face_plotter_inst
 
     def set_face_plotter(self, face_plotter_inst):
+        """
+        Umożliwia wymianę lub ponowne ustawienie instancji FacePlotter (np. w sytuacji
+        ponownej inicjalizacji wizualizacji 3D).
+
+        :param face_plotter_inst: Nowa instancja FacePlotter.
+        :type face_plotter_inst: FacePlotter
+        """
         self.face_plotter_inst = face_plotter_inst
 
     def queue_parameters(self, mar, is_yawning, roll, pitch, ear, perclos, yawn_counter, fps):
+        """
+        Dodaje zestaw parametrow (m.in. MAR, EAR, PERCLOS, FPS) do kolejki data_queue,
+        skąd będą pobrane i wyświetlane w metodzie update_labels().
+
+        :param mar: Mouth Aspect Ratio (MAR).
+        :type mar: float
+        :param is_yawning: Informacja, czy aktualnie wykryto ziewanie.
+        :type is_yawning: bool
+        :param roll: Kąt przechyłu głowy (roll).
+        :type roll: float
+        :param pitch: Kąt pochylenia głowy (pitch).
+        :type pitch: float
+        :param ear: Eye Aspect Ratio (EAR).
+        :type ear: float
+        :param perclos: Wskaźnik PERCLOS (procent czasu, gdy oczy są zamknięte).
+        :type perclos: float
+        :param yawn_counter: Licznik ziewnięć w obecnej sesji.
+        :type yawn_counter: int lub float
+        :param fps: Liczba klatek na sekundę (Frames Per Second).
+        :type fps: float lub int
+        """
         self.data_queue.put((mar, is_yawning, roll, pitch, ear, perclos, yawn_counter, fps))
 
     def change_appearance(self, new_appearance_mode):
+        """
+        Zmienia styl (jasny/ciemny/systemowy) interfejsu za pomocą funkcji
+        biblioteki customtkinter. Jednocześnie aktualizuje obraz tła w zależności
+        od wybranego stylu.
+
+        :param new_appearance_mode: Wybrany styl: "Jasny", "Ciemny" lub "Systemowy".
+        :type new_appearance_mode: str
+        """
         mode_dict = {"Jasny": "Light", "Ciemny": "Dark", "Systemowy": "System"}
         customtkinter.set_appearance_mode(mode_dict[new_appearance_mode])
 
@@ -207,14 +278,24 @@ class GUI:
         else:
             bg_image_path = self.current_path + "/gray_bg_img.jpeg"
 
-        # Update the background image
-        self.bg_image = customtkinter.CTkImage(Image.open(bg_image_path), size=(self.width, self.height))
+        # Aktualizacja obrazka tła.
+        self.bg_image = customtkinter.CTkImage(
+            Image.open(bg_image_path),
+            size=(self.width, self.height)
+        )
         self.bg_image_label.configure(image=self.bg_image)
 
     def update_webcam(self):
+        """
+        Cykl aktualizacji obrazu w oknie 'Kamera'. Metoda co około 33 ms (30 FPS)
+        pobiera z kolejki najnowszy obraz, konwertuje go do formatu RGB i wyświetla
+        na płótnie (Canvas). Następnie ponownie planuje wywołanie samej siebie
+        (window.after()).
+        """
         try:
             while not self.image_queue.empty():
                 image = self.image_queue.get_nowait()
+                # Konwersja z BGR do RGB (OpenCV -> PIL).
                 image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
                 self.current_image = ImageTk.PhotoImage(image)
                 self.webcam_canvas.create_image(0, 0, image=self.current_image, anchor="nw")
@@ -223,9 +304,16 @@ class GUI:
             pass
 
         if self.running:
+            # Zaplanowanie ponownego wywołania po 33 ms (ok. 30 FPS).
             self.window.after(33, self.update_webcam)
 
     def update_labels(self):
+        """
+        Cykl aktualizacji etykiety z parametrami w panelu 'Parametry'.
+        Metoda pobiera z kolejki data_queue najnowszy zestaw obliczonych
+        parametrów i wyświetla je w etykiecie params_label. Następnie
+        ponownie planuje wywołanie samej siebie (window.after()).
+        """
         try:
             while not self.data_queue.empty():
                 mar, is_yawning, roll, pitch, ear, perclos, yawn_counter, fps = self.data_queue.get_nowait()
@@ -246,7 +334,12 @@ class GUI:
             pass
 
         if self.running:
+            # Zaplanowanie ponownego wywołania po 100 ms.
             self.window.after(100, self.update_labels)
 
     def start(self):
+        """
+        Rozpoczyna główną pętlę zdarzeń Tkinter, dzięki czemu interfejs użytkownika
+        pozostaje aktywny i reaguje na działania (np. odświeżanie obrazu, klikanie przycisków).
+        """
         self.window.mainloop()
